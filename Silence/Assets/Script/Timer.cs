bcae8891fe1ba804871f43c8e9e02f09931c6558
Jonathan Schonfeld <jerrycritic@gmail.com>
2017-01-16 12:45:35 -0500

    Big Boss is back
    Big Boss is back

--- Silence/Assets/Script/Timer.cs
+++ Silence/Assets/Script/Timer.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class Timer : MonoBehaviour {
 
-	private float seconds = 0.0f;
-	private float minutes = 0.0f;
-	private float hours = 0.0f;
+	//private float seconds = 0.0f;
+	//private float minutes = 0.0f;
+	//private float hours = 0.0f;
 
 	// Use this for initialization
 	void Start () {
